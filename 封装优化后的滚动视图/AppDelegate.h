505122d40135c063233d4bc32572a569169d22e5
liuyujing <liuyujingmusic@163.com>
2015-12-14 12:16:25 +0800

    Initial Commit

---
+++ 封装优化后的滚动视图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  封装优化后的滚动视图
+//
+//  Created by Bruce on 15/12/14.
+//  Copyright © 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 封装优化后的滚动视图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  封装优化后的滚动视图
+//
+//  Created by Bruce on 15/12/14.
+//  Copyright © 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
